67f4e15562b570befc857ef7d256aaea65812307
Miisane <azarmisan@gmail.com>
2025-06-03 14:54:42 +0800

    menu game mode

---
+++ Private/MenuGameMode.cpp
@@ -0,0 +1,48 @@
+﻿#include "MenuGameMode.h"
+#include "Kismet/GameplayStatics.h"
+#include "Camera/CameraActor.h"
+
+AMenuGameMode::AMenuGameMode()
+{
+	MenuCamera = nullptr;
+}
+
+void AMenuGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+    
+	if (MenuCamera)
+	{
+		SetMenuCamera();
+	}
+	else
+	{
+		FindAndSetMenuCamera();
+	}
+}
+
+void AMenuGameMode::SetMenuCamera()
+{
+	if (MenuCamera)
+	{
+		APlayerController* PC = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+		if (PC)
+		{
+			PC->SetViewTarget(MenuCamera);
+            
+			PC->SetInputMode(FInputModeUIOnly());
+			PC->bShowMouseCursor = true;
+		}
+	}
+}
+
+void AMenuGameMode::FindAndSetMenuCamera()
+{
+	// Find camera by name
+	AActor* FoundCamera = UGameplayStatics::GetActorOfClass(GetWorld(), ACameraActor::StaticClass());
+	if (FoundCamera)
+	{
+		MenuCamera = Cast<ACameraActor>(FoundCamera);
+		SetMenuCamera();
+	}
+}
---
+++ Public/MenuGameMode.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Camera/CameraActor.h"
+#include "MenuGameMode.generated.h"
+
+UCLASS()
+class FIRSTPERSONTEST_API AMenuGameMode : public AGameModeBase
+{
+    GENERATED_BODY()
+
+public:
+    AMenuGameMode();
+
+protected:
+    virtual void BeginPlay() override;
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Menu Camera")
+    ACameraActor* MenuCamera;
+
+    UFUNCTION(BlueprintCallable, Category = "Menu Camera")
+    void SetMenuCamera();
+
+    UFUNCTION(BlueprintCallable, Category = "Menu Camera")
+    void FindAndSetMenuCamera();
+};
